b7741343add667ac44c44516f32c9951f9d1b2d4
kaysea-101 <kristina.bognot@gmail.com>
2024-02-20 10:25:39 +0800

    day 13 rest api reads csv file and return a json response

--- day-13/Submission/main.py
+++ day-13/Submission/main.py
@@ -1,15 +1,22 @@
-from flask import Flask, jsonify  
+from flask import Flask, jsonify
+import csv
 
 app = Flask(__name__)
 
-csv_file = 'programming_languages.csv'
-
-def read_csv():
-    pass
-
+# route for get request
 @app.route('/', methods=['GET'])
-def get_langs():
-    pass
+def get_csv_data():
+    csv_file_path = 'programming_languages.csv'  
+
+    # open csv
+    with open(csv_file_path, 'r') as file:
+        # read csv file using DictReader
+        reader = csv.DictReader(file)
+        # convert to list
+        data = list(reader)
+
+    # return the data in JSON format
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
